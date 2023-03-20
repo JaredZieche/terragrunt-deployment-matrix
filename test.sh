c610dd767c3d061cb09c740196b8bb90e9d3152f
jaredzieche <jaredzieche@gmail.com>
2023-03-20 09:55:17 -0400

    updating test workflow to create files for passing input checks

--- test.sh
+++ test.sh
@@ -3,22 +3,17 @@
 touch /tmp/ghoutput
 touch /tmp/summary.md
 
-export GITHUB_OUTPUT="/tmp/ghoutput"
-export GITHUB_STEP_SUMMARY="/tmp/summary.md"
-export TEST=true
-
-export INPUT_BASE_DIRECTORY="examples/src/terraform"
-export INPUT_PROVIDERS="aws|azure"
-export INPUT_ENVIRONMENTS="sbx|dev|stage|prod"
-export INPUT_REGIONS="us-west-1|us-east-1|us-west-2|us-east-2"
-export INPUT_RESOURCE_GROUPS="cluster|lambda"
-export INPUT_GLOBAL_FILES='["examples/src/terraform/aws/terragrunt.hcl", "examples/src/terraform/aws/global.hcl"]'
+echo 'GITHUB_OUTPUT="/tmp/ghoutput"' >> .env
+echo 'GITHUB_STEP_SUMMARY="/tmp/summary.md"' >> .env
+echo 'INPUT_BASE_DIRECTORY="examples/src/terraform"' >> .env
+echo 'INPUT_PROVIDERS="aws|azure"' >> .env
+echo 'INPUT_ENVIRONMENTS="sbx|dev|stage|prod"' >> .env
+echo 'INPUT_REGIONS="us-west-1|us-east-1|us-west-2|us-east-2"' >> .env
+echo 'INPUT_RESOURCE_GROUPS="cluster|lambda"' >> .env
+echo 'INPUT_GLOBAL_FILES='["examples/src/terraform/aws/terragrunt.hcl", "examples/src/terraform/aws/global.hcl"]'' >> .env
 
 printf -v joined '"%s", ' $(cat examples/standard.txt)
 
-export INPUT_FILES=$(echo "[${joined%s,}\"ignore\"]")
+echo 'INPUT_FILES=$(echo "[${joined%s,}\"ignore\"]")' >> .env
 
 docker run -v src:/src/ --rm -it --platform=linux/arm64 tg-action:latest
-
-cat $GITHUB_OUTPUT
-cat $GITHUB_STEP_SUMMARY
